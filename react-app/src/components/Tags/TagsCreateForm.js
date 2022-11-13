38b4bc4bbcbea467c44361651f5a08cb6d8936bb
Kevin Longboy <kevinlongboy@gmail.com>
2022-11-13 00:06:59 -0800

    Two more components initialized.

---
+++ react-app/src/components/Tags/TagsCreateForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TagCreateForm() {
+
+    return (
+        <div className='page-wrapper-container'>
+            <h1>TagCreateForm component waiting to be built!</h1>
+        </div>
+    )
+}
+
+
+export default TagCreateForm
