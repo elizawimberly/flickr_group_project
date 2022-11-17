dff7e3dfe6d1c39e7cd326084c24f0c9f828a26d
Kevin Longboy <kevinlongboy@gmail.com>
2022-11-16 19:52:05 -0800

    Push before merge

--- react-app/src/components/Account/index.js
+++ react-app/src/components/Account/index.js
@@ -8,6 +8,7 @@ import './Account.css'
 import coverphoto from '../../assets/coverphoto.jpg'
 import profilePic from '../../assets/buddyicon.png';
 import { thunkReadAllPhotosByUser } from "../../store/photosReducer";
+import { Redirect } from "react-router-dom";
 
 
 /******************************* COMPONENT *******************************/
@@ -28,6 +29,8 @@ function Account() {
     },[dispatch])
 
     /**************** render component *****************/
+    if (!sessionUser) return <Redirect to="/" />;
+
     return (
         <div className='page-wrapper-container'>
             <div className="account-component"
@@ -46,6 +49,7 @@ function Account() {
                             <img src={profilePic} className="account-profile-pic"></img>
                         </div>
                         <div className="account-profile-names-container">
+                            {/* <h1 id="banner-user-full-name">{sessionUser.firstName} {sessionUser.lastName}</h1> */}
                             <h1 id="banner-user-full-name">{sessionUser.first_name} {sessionUser.last_name}</h1>
                             <p id="banner-user-username">{sessionUser.username}</p>
                         </div>
