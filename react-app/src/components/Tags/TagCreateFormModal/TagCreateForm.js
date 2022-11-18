6912d8a0db295b80dcd9dd379fb1ae3c2fc40401
Eliza <elizawimberly@gmail.com>
2022-11-17 21:24:36 -0800

    added react constraints

--- react-app/src/components/Tags/TagCreateFormModal/TagCreateForm.js
+++ react-app/src/components/Tags/TagCreateFormModal/TagCreateForm.js
@@ -1,54 +1,48 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {thunkCreateSingleTag} from '../../../store/photosReducer'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { thunkCreateSingleTag } from "../../../store/photosReducer";
 
-function TagCreateForm({onClose}) {
+function TagCreateForm({ onClose }) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const photosState = useSelector((state) => state.photos);
 
-    const photosState = useSelector((state) => state.photos);
+  const photo = photosState.singlePhotoDetails;
 
-    const photo = photosState.singlePhotoDetails;
+  const [tags, setTags] = useState("");
 
-    const [tags, setTags] = useState("");
+  const createTag = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(thunkCreateSingleTag(photo.id, tags));
+  };
 
-    const createTag = async (e) => {
-        e.preventDefault()
-        const data = await dispatch(thunkCreateSingleTag(photo.id, tags));
-    };
+  return (
+    <div className="page-wrapper-container">
+      <div className="modal-container">
+        <p className="modal-title">Add tags</p>
 
-
-    return (
-        <div className='page-wrapper-container'>
-            <div className='modal-container'>
-
-                <p className='modal-title'>Add tags</p>
-
-                <div className='modal-body'>
-
-                    <form
-                        className="modal-form"
-                        onSubmit={createTag}>
-                        <label>
-                            <textarea
-                            className="modal-textarea-input-field"
-                            type="text"
-                            name="tags"
-                            placeholder="Add tags"
-                            onChange={(e) => setTags(e.target.value)}
-                            required={true}
-                            maxLength={500}
-                            value={tags}
-                            ></textarea>
-                        </label>
-                        <button className="modal-button" type='submit'>Submit</button>
-                    </form>
-
-                </div>
-            </div>
+        <div className="modal-body">
+          <form className="modal-form" onSubmit={createTag}>
+            <label>
+              <textarea
+                className="modal-textarea-input-field"
+                type="text"
+                name="tags"
+                placeholder="Add tags"
+                onChange={(e) => setTags(e.target.value)}
+                minLength={2}
+                maxLength={50}
+                value={tags}
+              ></textarea>
+            </label>
+            <button className="modal-button" type="submit">
+              Submit
+            </button>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-
-export default TagCreateForm
+export default TagCreateForm;
